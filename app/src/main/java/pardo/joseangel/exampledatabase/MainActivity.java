e1fafeafe9d859e8747532353300add0e0ccee76
Jose Angel Pardo Cerdán <joseangelpardocerdan@hotmail.com>
2015-05-30 22:18:41 +0200

    Ejemplo de creación de base de datos SQLite en android

---
+++ app/src/main/java/pardo/joseangel/exampledatabase/MainActivity.java
@@ -0,0 +1,104 @@
+package pardo.joseangel.exampledatabase;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+import java.util.Random;
+
+import DataBase.Comment;
+import DataBase.CommentsDataSource;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+
+    private CommentsDataSource datasource;
+    private ListView list;
+    ArrayAdapter<Comment> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        datasource = new CommentsDataSource(this);
+        datasource.open();
+        List<Comment> values = datasource.getAllComments();
+
+        // use the SimpleCursorAdapter to show the
+        // elements in a ListView
+        adapter = new ArrayAdapter<Comment>(this,
+                android.R.layout.simple_list_item_1, values);
+        //setListAdapter(adapter);
+
+        list =(ListView) findViewById(R.id.listView);
+        list.setAdapter(adapter);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        @SuppressWarnings("unchecked")
+        //ArrayAdapter<Comment> adapter = (ArrayAdapter<Comment>) getListAdapter();
+        Comment comment = null;
+        switch (view.getId()) {
+            case R.id.add:
+                String[] comments = new String[] { "Cool", "Very nice", "Hate it" };
+                int nextInt = new Random().nextInt(3);
+                // save the new comment to the database
+                comment = datasource.createComment(comments[nextInt]);
+                adapter.add(comment);
+                break;
+            case R.id.delete:
+                if (adapter.getCount() > 0) {
+                    comment = (Comment) adapter.getItem(0);
+                    datasource.deleteComment(comment);
+                    adapter.remove(comment);
+                }
+                break;
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onResume() {
+        datasource.open();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        datasource.close();
+        super.onPause();
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
